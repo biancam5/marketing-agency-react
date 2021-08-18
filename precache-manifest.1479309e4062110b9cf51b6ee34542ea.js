8fb63a6447716cbc2173abc15114f5c055a3a844
biancam5 <biancamainella@gmail.com>
2021-08-18 20:09:44 -0300

    Updates

---
+++ precache-manifest.1479309e4062110b9cf51b6ee34542ea.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "167eb684a29930e84c9b8481681e55fa",
+    "url": "/marketing-agency-react/index.html"
+  },
+  {
+    "revision": "242fcf86c46881dbd310",
+    "url": "/marketing-agency-react/static/js/2.23c15765.chunk.js"
+  },
+  {
+    "revision": "1e7ce0b5e5c5dbbe2385af5c63cacff7",
+    "url": "/marketing-agency-react/static/js/2.23c15765.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "763005a84a35b4f4c636",
+    "url": "/marketing-agency-react/static/js/main.e683d9d5.chunk.js"
+  },
+  {
+    "revision": "e537f22be6c616278374",
+    "url": "/marketing-agency-react/static/js/runtime-main.1a06fa5e.js"
+  },
+  {
+    "revision": "284cf86c82376eea2d24286d32131a06",
+    "url": "/marketing-agency-react/static/media/profile.284cf86c.jpg"
+  },
+  {
+    "revision": "1044a68b576ac1e2bb5b4609e1b594b7",
+    "url": "/marketing-agency-react/static/media/svg-1.1044a68b.svg"
+  },
+  {
+    "revision": "80814e39c8e8d9444abde132903e75d8",
+    "url": "/marketing-agency-react/static/media/svg-2.80814e39.svg"
+  },
+  {
+    "revision": "8786eabcca31b9ac2febd8fbd80f4375",
+    "url": "/marketing-agency-react/static/media/svg-3.8786eabc.svg"
+  }
+]);
